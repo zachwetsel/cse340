6acf93d132bdbe8e19dbbc281cd2152f86f4cfc5
zachwetsel <zachwetsel@yahoo.com>
2024-05-11 16:46:52 -0600

    bug fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
  * Require Statements
  *************************/
 const express = require("express")
-const expresslayouts = require("express-ejs-layouts")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
@@ -21,6 +21,10 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
+// Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
