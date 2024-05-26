0454c046b2125293551a7445888378bf7b17ffa5
zachwetsel <zachwetsel@yahoo.com>
2024-05-25 19:23:19 -0600

    Finished Week 3 Learning Activities

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -19,4 +19,4 @@ WHERE c.classification_name = 'Sport';
 
 UPDATE inventory
 SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
-inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles');
+inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/")
 /* ***********************
  * View Engine and Templates
  *************************/
@@ -22,10 +24,27 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
+app.get("/", utilities.handleErrors(baseController.buildHome))
+// Inventory routes
+app.use("/inv", inventoryRoute)
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
-
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
